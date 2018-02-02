a5d5213460ba06f99f5cead8ab29eb92d955f557
George Agadah <Tobe@Dead-Band.local>
2018-02-01 23:37:36 -0500

    second commit

---
+++ app/src/main/java/uottawa/engineering/simplerentcalculator/summaryPage.java
@@ -0,0 +1,13 @@
+package uottawa.engineering.simplerentcalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class summaryPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_summary_page);
+    }
+}
